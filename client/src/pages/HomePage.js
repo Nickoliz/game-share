0b5e0d8eafed1dbb039e68ecb038fcf890292320
nick <nlitzc7@gmail.com>
2020-10-06 17:05:57 -0700

    homepage started - starting new branch

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -6,7 +6,36 @@ export default function HomePage() {
 
   return (
     <>
-
+      <div className='homepage_main_container'>
+        <div className='homepage_logo-container' />
+        <div className='homepage_nav-container'>
+          <div className='homepage_nav-button'>
+            <div id='homepage_nav-button-box'>
+              Discover
+            </div>
+          </div>
+          <div className='homepage_nav-button'>
+            <div id='homepage_nav-button-box'>
+              Discover
+            </div>
+          </div>
+          <div className='homepage_nav-button'>
+            <div id='homepage_nav-button-box'>
+              Discover
+            </div>
+          </div>
+          <div className='homepage_nav-button'>
+            <div id='homepage_nav-button-box'>
+              Discover
+            </div>
+          </div>
+          <div className='homepage_nav-button'>
+            <div id='homepage_nav-button-box'>
+              Discover
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
