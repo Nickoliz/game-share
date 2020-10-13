16ba4f7d47e464f2ffc4857e5ac7818e965398b0
nick <nlitzc7@gmail.com>
2020-10-13 12:07:07 -0700

    backend routes ready to test

--- client/src/store/atlas.js
+++ client/src/store/atlas.js
@@ -1,6 +1,7 @@
 const GET_ORDERBY_GAMES = 'atlas/get_order_by';
 const GET_POPULAR_GAMES = 'atlas/get_popular_games'
 const GET_REDDIT_GAMES = 'atlas/get_reddit_games'
+const GET_GAMES_FOR_ADD_SEARCH = 'atlas/get_games_for_add_search'
 
 const {
   atlas: { client_id },
@@ -28,6 +29,12 @@ export const getRedditGames = (games) => {
   };
 };
 
+export const loadAddSearchGames = (games) => {
+  return {
+    type: GET_GAMES_FOR_ADD_SEARCH,
+    games: games
+  }
+}
 
 export const loadOrderByGames = category => {
   return async dispatch => {
@@ -39,12 +46,25 @@ export const loadOrderByGames = category => {
       }
       return res;
     } catch (err) {
-      console.warn(err)
+      console.warn("Error: ", err)
     }
   }
 }
 
-
+export const loadGamesForSearch = searchTerm => {
+  return async dispatch => {
+    try {
+      const res = await fetch(`https://api.boardgameatlas.com/api/search?name=${searchTerm}&limit=6&client_id=${client_id}`)
+      res.data = await res.json()
+      if (res.ok) {
+        return dispatch(loadAddSearchGames(res.data.games))
+      }
+      return res;
+    } catch (err) {
+      console.warn("Error: ", err)
+    }
+  }
+}
 
 
 // FOR FLASK REQUESTS
@@ -66,6 +86,8 @@ export default function atlasReducer(state = {}, action) {
   switch (action.type) {
     case GET_ORDERBY_GAMES:
       return { ...state, orderByGames: action.games };
+    case GET_GAMES_FOR_ADD_SEARCH:
+      return {...state, gamesForSearch: action.games}
     default:
       return state;
   }
