0d428a6268c1c68fa722d8ff5b6214b07176150b
nick <nlitzc7@gmail.com>
2020-10-18 22:03:05 -0700

    static

---
+++ backend/app/static/precache-manifest.d231e38c7d0a787c991f4e7396c84d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569c2dedcea7fc231263320b2cea6ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dc7d1ade311c0e6178b",
+    "url": "/static/css/main.05bc4097.chunk.css"
+  },
+  {
+    "revision": "c23945ef1d9404272068",
+    "url": "/static/js/2.e6e43e65.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e6e43e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc7d1ade311c0e6178b",
+    "url": "/static/js/main.7a545573.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
