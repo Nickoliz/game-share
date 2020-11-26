dc421e8f0ba514abaad756ee2e594b1972bfcb64
nick <nlitzc7@gmail.com>
2020-11-26 12:55:14 -0800

    updated static for build

---
+++ backend/app/static/precache-manifest.8d2ed810c55d8ef42b484ab4c3fd7aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd26e2007cc497ac9979ee3557627531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74d8d9b3e503732f36c6",
+    "url": "/static/css/main.62fd5480.chunk.css"
+  },
+  {
+    "revision": "d0a0053d745039707269",
+    "url": "/static/js/2.2dc2951c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2dc2951c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d8d9b3e503732f36c6",
+    "url": "/static/js/main.198d4e42.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
